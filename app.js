400258cd332e927632d851e22a02dc4abda3de4d
annedykstra <anne.j.dykstra@gmail.com>
2020-08-06 11:53:57 +0200

    add port

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/albums", albumRoutes);
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
     console.log('Server has started!');
 });
